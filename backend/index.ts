1b1810f028b45455f01bf692f496b5ddefc24165
Rian Brunelli <rian10brunelli@gmail.com>
2022-05-09 21:40:22 -0300

    Subindo projeto

---
+++ backend/index.ts
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+//Config JSON responde
+app.use(express.json());
+
+//Solve Cors
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+
+//Public folder for images
+app.use(express.static('public'));
+
+//Routes
+
+
+app.listen(5000);
