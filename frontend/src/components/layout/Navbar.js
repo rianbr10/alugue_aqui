a8c20f255e2a04727beb7f80c09c8234bbab14ea
Rian Brunelli <rian10brunelli@gmail.com>
2022-07-16 20:13:34 -0300

    Persistindo autenticação no front

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -1,10 +1,16 @@
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { useContext } from 'react';
 
 import Logo from '../../assets/img/logo.png';
 
 import styles from './Navbar.module.css';
 
+import { Context } from '../../context/UserContext';
+
 function Navbar() {
+
+  const { authenticated } = useContext(Context)
+
   return (
     <nav className={styles.navbar}>
       <div className={styles.navbar_logo}>
@@ -15,12 +21,22 @@ function Navbar() {
         <li>
           <Link to="/">Alugar</Link>
         </li>
-        <li>
-          <Link to="/login">Entrar</Link>
-        </li>
-        <li>
-          <Link to="/register">Cadastrar</Link>
-        </li>
+        {authenticated ? (
+          <>
+            <p>Logado</p>
+          </>
+        ) : (
+          <>
+            <li>
+              <Link to="/login">Entrar</Link>
+            </li>
+            <li>
+              <Link to="/register">Cadastrar</Link>
+            </li>
+          </>
+        )
+        }
+
       </ul>
     </nav>
   )
